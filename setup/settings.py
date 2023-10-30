5998c4d9859c002742860f50c29b87ca34fab403
André Ribeiro <andre88br@gmail.com>
2023-10-30 15:01:33 -0300

    incluindo configuração do banco de dados

--- setup/settings.py
+++ setup/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # Database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "l6r7w588",
+        "HOST": "postgres.czb5uis4b2yu.us-east-1.rds.amazonaws.com",
     }
 }
 
